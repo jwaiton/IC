677ae6e5f193e6ad2d14c8c1082ed23b6a202211
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2026-03-18 16:49:48 +0000

    call dictionary correctly

--- invisible_cities/cities/thekla.py
+++ invisible_cities/cities/thekla.py
@@ -176,11 +176,11 @@ def thekla(    files_in         : OneOrManyFiles
 
     with tb.open_file(file_out, 'w', filters=tbl.filters(compression)) as h5out:
         write_event_info = fl.sink(run_and_event_writer(h5out), args=("run_number", "event_number", "timestamp"))
-        write_hits       = fl.sink(hits_writer(h5out, group_name = hit_location.group_name, table_name = hit_location.table_name), args="hits")
+        write_hits       = fl.sink(hits_writer(h5out, group_name = hit_location['group_name'], table_name = hit_location['table_name']), args="hits")
         write_kdst_table = fl.sink( kdst_from_df_writer(h5out), args =  "kdst"      )
         compute_tracks   = compute_and_write_tracks_info(paolina_params, h5out, hit_type=energy_type)
         # it should write the reco'd hits and also the topological information
-        result = push(source = hits_and_kdst_from_files(files_in, hit_location.group_name, hit_location.table_name),
+        result = push(source = hits_and_kdst_from_files(files_in, hit_location['group_name'], hit_location['table_name']),
                       pipe   = pipe(fl.slice(*event_range, close_all=True)  ,
                                     print_every(print_mod)                  ,
                                     event_count_in.spy                      ,
